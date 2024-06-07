4351fff8310585813d6065c98273abfda9848445
kimseokhee <cat1181123@naver.com>
2024-06-07 21:24:44 +0900

    Add: cors

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,20 @@
+import axios from "axios";
+
 export default function Home() {
-  return <div>안녕</div>;
+  const handleClickButton = async () => {
+    try {
+      const response = await axios.post(
+        "https://next-api-bay-zeta.vercel.app/api/login",
+        {
+          email: "seokiis@naver.com",
+          password: "1234",
+        }
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return <button onClick={handleClickButton}>Click me!</button>;
 }
