ce97091b4042e08735e199800e91acc66e560cb3
kimseokhee <cat1181123@naver.com>
2024-06-07 21:54:25 +0900

    hotFix: 허용 url 변경

--- next.config.mjs
+++ next.config.mjs
@@ -6,10 +6,19 @@ const nextConfig = {
     return [
       {
         // matching all API routes
+
         source: "/api/:path*",
+
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "http://localhost:3000",
+          },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "http://localhost:3001",
+          },
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
